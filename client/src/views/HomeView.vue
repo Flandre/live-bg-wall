637b5bd2daf923cbc3fc000d287c4b570b53cdc8
Flandre <flantan233@gmail.com>
2024-03-15 16:26:43 +0800

    init front wall

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Wave from '../components/bg/Wave.vue'
+</script>
+
+<template>
+  <div class="home">
+    <Wave class="wave-container"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  width: 1920px;
+  height: 1080px;
+  background-color: rgba(0,0,0,1);
+  position: relative;
+  .wave-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
