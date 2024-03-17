6c03cccde8e233e3e781551350ebdb198fd3f94d
Flandre <flantan233@gmail.com>
2024-03-17 14:23:55 +0800

    update theme

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,30 +6,46 @@ body      1920  1080
 comment   435   650
 ba-c      1425  100
 game      1425  800
-
 -->
 <template>
   <div class="home">
     <Wave class="wave-container"/>
     <div class="comment-container">
-      <div class="comment-box">
-
-      </div>
-      <div class="comment-title">> COMMENT</div>
+      <div class="comment-title">COMMENT</div>
     </div>
-    <div class="game-container-before-comment"></div>
+    <div class="game-container-before-comment">我已经尽力了 . JPG</div>
     <div class="game-container"></div>
-    <div class="game-container-after-comment"></div>
+    <div class="game-container-after-comment">阿巴阿巴阿巴阿巴阿巴</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+$bg-color: rgb(4, 6, 29);
+$primary: rgb(100,100,236);
+$primary-a1: rgb(100,100,236, .1);
+$primary-a4: rgb(100,100,236, .4);
+$box-main: rgb(124, 124, 246);
 .home {
   width: 1920px;
   height: 1080px;
-  background-color: rgba(23,23,23,1);
+  background-color: $bg-color;
   position: relative;
   overflow: hidden;
+  &:before {
+    content: '';
+    display: block;
+    width: 0px;
+    height: 0px;
+    border: 800px solid $primary-a1;
+    border-left-width: 1500px;
+    border-right-width: 1500px;
+    border-radius: 50% 70%;
+    box-shadow: 0 0 250px 200px $primary-a4;
+    position: absolute;
+    right: -1500px;
+    top: -800px;
+    box-sizing: border-box;
+  }
   .wave-container {
     position: absolute;
     top: 400px;
@@ -37,18 +53,33 @@ game      1425  800
   }
   .comment-container {
     width: 435px;
-    height: 650px;
-    background-color: #444;
+    height: 640px;
+    background-color: $box-main;
     position: absolute;
     right: 20px;
-    top: 20px;
-    .comment-title {
+    top:40px;
+    border-radius: 20px;
+    &:before {
+      content: '';
+      display: block;
+      width: 425px;
+      height: 630px;
+      border: 2px dashed #fff;
+      border-radius: 20px;
       position: absolute;
-      background-color: #fff;
-      width: 435px;
+      top: 5px;
+      left: 5px;
+    }
+    .comment-title {
+      display: block;
+      color: #fff;
+      position: absolute;
+      top: -40px;
+      font-size: 60px;
+      line-height: 1;
       padding: 10px 20px;
-      font-size: 30px;
       font-weight: bolder;
+      text-shadow: 3px 3px 20px #000;
     }
   }
   .game-container {
@@ -59,20 +90,35 @@ game      1425  800
     left: 20px;
     top: 140px;
   }
-  .game-container-before-comment {
+  .game-container-before-comment,
+  .game-container-after-comment{
     width: 1425px;
     height: 100px;
-    background-color: #444;
+    background-color: $box-main;
     position: absolute;
     left: 20px;
     top: 20px;
+    border-radius: 20px;
+    font-size: 50px;
+    color: #fff;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    &:before {
+      content: '';
+      display: block;
+      width: 1415px;
+      height: 90px;
+      border: 2px dashed #fff;
+      border-radius: 20px;
+      position: absolute;
+      top: 5px;
+      left: 5px;
+    }
   }
   .game-container-after-comment {
-    width: 1425px;
-    height: 100px;
-    background-color: #444;
-    position: absolute;
-    left: 20px;
+    top: auto;
     bottom: 20px;
   }
 }
