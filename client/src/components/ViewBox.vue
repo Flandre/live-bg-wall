a0ea82652f81a536fb1cae917a8879747d2b2614
Flandre <flantan233@gmail.com>
2024-03-18 17:31:12 +0800

    export theme.scss & export ViewBox component

---
+++ client/src/components/ViewBox.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div
+    class="view-box"
+    :style="{ width: width + 'px', height: height + 'px' }"
+  >
+    <div
+      class="view-border"
+      :style="{ width: width - 10 + 'px', height: height - 10 + 'px' }"
+    ></div>
+    <div class="title">{{ title }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.view-box {
+  width: 1425px;
+  height: 100px;
+  background-color: $box-main;
+  border-radius: 20px;
+  font-size: 50px;
+  color: #fff;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  .view-border {
+    border: 2px dashed #fff;
+    border-radius: 20px;
+    position: absolute;
+    top: 5px;
+    left: 5px;
+  }
+  .title {
+    display: block;
+    color: #fff;
+    position: absolute;
+    top: -20px;
+    font-size: 40px;
+    line-height: 1;
+    padding: 0 10px;
+    font-weight: bolder;
+    text-shadow: 3px 3px 20px #000;
+  }
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import Wave from '../components/bg/Wave.vue'
+import Wave from '@/components/bg/Wave.vue'
+import ViewBox from '@/components/ViewBox.vue'
 </script>
 <!--
 body      1920  1080
@@ -12,38 +13,38 @@ game      1425  800
 <template>
   <div class="home">
     <Wave class="wave-container"/>
-    <div class="comment-container">
-      <div class="comment-title">COMMENT</div>
-    </div>
-    <div class="game-container-before-comment">
-      <div class="title">INFO</div>
+    <ViewBox
+      width="435"
+      height="640"
+      title="COMMENT"
+      class="comment-container"
+    >
+    </ViewBox>
+    <ViewBox
+      width="1425"
+      height="80"
+      title="INFO"
+      class="game-container-before-comment"
+    >
       <marquee scrollamount="10">阿巴阿巴阿巴</marquee>
-    </div>
-    <div class="game-container"></div>
-    <div class="game-container-after-comment">
-      <div class="title">SPECTRALIZER</div>
-    </div>
+    </ViewBox>
+    <ViewBox
+      width="1425"
+      height="800"
+      class="game-container"
+    >
+    </ViewBox>
+    <ViewBox
+      width="1425"
+      height="100"
+      class="game-container-after-comment"
+      title="SPECTRALIZER"
+    >
+    </ViewBox>
   </div>
 </template>
 
 <style lang="scss" scoped>
-$bg-color: rgb(4, 6, 29);
-$primary: rgb(100,100,236);
-$primary-a1: rgb(100,100,236, .1);
-$primary-a4: rgb(100,100,236, .4);
-$box-main: rgba(124, 124, 246, .5);
-
-//$bg-color: rgb(252, 202, 210);
-//$primary: rgb(248, 63, 95);
-//$primary-a1: rgb(248, 63, 95, .1);
-//$primary-a4: rgb(248, 63, 95, .4);
-//$box-main: rgb(252, 106, 130);
-
-//$bg-color: rgb(4, 6, 29);
-//$primary: rgb(0,33,255);
-//$primary-a1: rgb(0,33,255, .1);
-//$primary-a4: rgb(0,33,255, .4);
-//$box-main: rgb(0,33,255);
 .home {
   width: 1920px;
   height: 1080px;
@@ -71,13 +72,9 @@ $box-main: rgba(124, 124, 246, .5);
     left: 0;
   }
   .comment-container {
-    width: 435px;
-    height: 640px;
-    background-color: $box-main;
     position: absolute;
     right: 20px;
     top:40px;
-    border-radius: 20px;
     &:before {
       content: '';
       display: block;
@@ -101,78 +98,20 @@ $box-main: rgba(124, 124, 246, .5);
       text-shadow: 3px 3px 20px #000;
     }
   }
-  .game-container {
-    width: 1425px;
-    height: 800px;
-    background-color: $box-main;
-    border-radius: 20px;
-    position: absolute;
-    left: 20px;
-    top: 140px;
-    &:before {
-      content: '';
-      display: block;
-      width: 1415px;
-      height: 790px;
-      border: 2px dashed #fff;
-      border-radius: 20px;
-      position: absolute;
-      top: 5px;
-      left: 5px;
-    }
-  }
-  .game-container-before-comment,
-  .game-container-after-comment{
-    width: 1425px;
-    height: 100px;
-    background-color: $box-main;
-    position: absolute;
-    left: 20px;
-    top: 20px;
-    border-radius: 20px;
-    font-size: 50px;
-    color: #fff;
-    padding: 0 20px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    &:before {
-      content: '';
-      display: block;
-      width: 1415px;
-      height: 90px;
-      border: 2px dashed #fff;
-      border-radius: 20px;
-      position: absolute;
-      top: 5px;
-      left: 5px;
-    }
-    .title {
-      display: block;
-      color: #fff;
-      position: absolute;
-      top: -30px;
-      font-size: 50px;
-      line-height: 1;
-      padding: 10px 0;
-      font-weight: bolder;
-      text-shadow: 3px 3px 20px #000;
-    }
-  }
   .game-container-before-comment {
-    height: 80px;
+    position: absolute;
     top: 40px;
-    font-size: 40px;
-    &:before {
-      height: 70px;
-    }
-    .title {
-      top: -40px;
-    }
+    left: 20px;
+  }
+  .game-container {
+    position: absolute;
+    top: 140px;
+    left: 20px;
   }
   .game-container-after-comment {
-    top: auto;
+    position: absolute;
     bottom: 20px;
+    left: 20px;
   }
 }
 </style>
