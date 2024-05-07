177fa36a47865998f6e0750270adddc8cafa25f0
Flandre <flantan233@gmail.com>
2024-05-07 17:31:59 +0800

    update hack bg

--- client/src/components/bg/Hack.vue
+++ client/src/components/bg/Hack.vue
@@ -1,69 +1,62 @@
 <script setup>
-import {onMounted} from "vue";
+import {ref, reactive, onMounted, onBeforeUnmount} from "vue";
+
+const timer = ref(0)
+const fontSize = 18
+const columns = ref(0)
+const canvas = ref()
+const coordinate = reactive([])
+const str = "javascript html5 canvas Flandre is wonderful Bot Design baibai"
+
+const initBg = () => {
+  canvas.value.width = 1920
+  canvas.value.height = 1080
+  columns.value = Math.floor(canvas.value.width / fontSize)
+  for (let i = 0; i < columns.value; i++) {
+    coordinate.push(0);
+  }
+  draw()
+  timer.value = setInterval(draw, 30)
+}
+
+const draw = () => {
+  const ctx = canvas.value.getContext("2d")
+  const imageData = ctx.getImageData(0, 0, canvas.value.width, canvas.value.height);
+  let data = imageData.data;
+  for (let i = 3; i < data.length; i += 4) {
+    // 透明度值位于每个像素数据的第四个元素（索引为i）
+    data[i] = Math.max(0, data[i] - 5) // 减少透明度，但不小于0
+  }
+  ctx.putImageData(imageData, 0, 0)
+
+  ctx.font = "500 " + fontSize + "px  Mono"
+  ctx.fillStyle = "#ffffff"
+
+  // console.log(coordinate)
+  for (let i = 0; i < columns.value; i++) {
+    var index = Math.floor(Math.random() * str.length)
+    var x = i * fontSize;
+    var y = coordinate[i] * fontSize
+    ctx.fillText(str[index], x, y)
+    if (y >= canvas.value.height && Math.random() > 0.88) {
+      coordinate[i] = 0
+    }
+    coordinate[i]++
+  }
+}
+
 onMounted(() => {
   initBg()
 })
 
-const initBg = () => {
-  var canvas = document.getElementById("hack-box");
-  var context = canvas.getContext("2d");
-  // 浏览器宽高
-  // var W = window.screen.availWidth;
-  // var H = window.screen.availHeight;
-  var W = 1920;
-  var H = 1080;
-  canvas.width = W;
-  canvas.height = H;
-  var fontSize = 18;
-  var colunms = Math.floor(W / fontSize);
-  // 坐标
-  var coordinate = [];
-  for (var i = 0; i < colunms; i++) {
-    coordinate.push(0);
-  }
-  var str = "javascript html5 canvas Flandre is wonderful Bot Design baibai";
-  function draw() {
-    // 背景色
-    // context.fillStyle = "rgba(0,0,0,0.05)";
-    // context.fillStyle = "rgba(255,255,255,0.05)";
-
-      var imageData = context.getImageData(0, 0, W, H);
-      var data = imageData.data;
-
-      for (var i = 3; i < data.length; i += 4) {
-          // 透明度值位于每个像素数据的第四个元素（索引为i）
-          data[i] = Math.max(0, data[i] - 5); // 减少透明度，但不小于0
-      }
-
-      context.putImageData(imageData, 0, 0);
-
-    // context.fillStyle = "rgba(255,255,255,0)";
-    // context.globalAlpha = 0.05;
-    // context.fillRect(0, 0, W, H);
-    // context.globalAlpha = 1
-    context.font = "500 " + fontSize + "px  Mono";
-    // 字体颜色
-    context.fillStyle = "#ffffff";
-    for (var i = 0; i < colunms; i++) {
-      var index = Math.floor(Math.random() * str.length);
-      var x = i * fontSize;
-      var y = coordinate[i] * fontSize;
-      context.fillText(str[index], x, y);
-      if (y >= canvas.height && Math.random() > 0.88) {
-        coordinate[i] = 0;
-      }
-      coordinate[i]++;
-    }
-  }
-
-  draw();
-  setInterval(draw, 30);
-}
+onBeforeUnmount(() => {
+  clearTimeout(timer.value)
+})
 
 </script>
 
 <template>
-  <canvas id="hack-box"></canvas>
+  <canvas id="hack-box" ref="canvas"></canvas>
 </template>
 
 <style lang="scss" scoped>
